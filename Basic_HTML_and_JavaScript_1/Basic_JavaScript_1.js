6936c81388b6a0ff2d24ed14f731f0fb74207581
poymelo84 <jaime.melo@ymail.com>
2025-09-07 13:20:20 -0400

    Basic_HTML_and_JavaScript_1

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,8 @@
+alert("Don't click OK------------------------>");
+
+function My_First_Function() {
+		var String = "You've just won $1,000,000!";
+		var result = String.fontcolor("red") ;
+		document.getElementById("Irish").innerHTML =
+		result;
+	}
